b18b2f55653d32dfd73f0dc341e494b7c2d37d2f
fusheng <liqiang_gr@163.com>
2021-01-06 19:29:38 +0800

    无用代码

--- Topics/13.roman_to_integer/answers/ifushion.js
+++ Topics/13.roman_to_integer/answers/ifushion.js
@@ -23,29 +23,3 @@ var romanToInt = function(s) {
   }
   return res;
 }
-
-console.log(romanToInt('IIV'))
-
-
-function format(a, i) {
-  if (!a) return '';
-  if (a < 4) return arr[i].repeat(a);
-  if (a === 4) return `${arr[i]}${arr[i + 1]}`;
-  if (a === 5) return `${arr[i + 1]}`;
-  if (a < 9) return `${arr[i + 1]}${arr[i].repeat(a - 5)}`;
-  if (a === 9) return `${arr[i]}${arr[i + 2]}`;
-}
-
-var intToRoman = function (num) {
-  let i = 4;
-  let roman = '';
-  while (num > 0 && i-- > 0) {
-    const div = Math.pow(10, i);
-    const a = Math.floor(num / div);
-    num = num % div;
-    if (a) roman += format(a, 2 * i);
-  }
-  return roman;
-};
-
-// console.log(intToRoman(2900))
